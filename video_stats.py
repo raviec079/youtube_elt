32595300df1eddf5c4da44ebe65dd235a0625529
RNagesh_flexera <RNagesh@flexera.com>
2025-11-13 15:19:19 +0000

    function added to get video ids

--- video_stats.py
+++ video_stats.py
@@ -7,6 +7,7 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
     try:
@@ -27,8 +28,39 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+playlist_id = get_playlist_id()
+
+def get_videos_ids(playlist_id):
+    video_ids = []
+
+    pageToken = None
+    
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlist_id}&key={API_KEY}"
+
+    try:
+        while True:
+            url = base_url
+            if pageToken:
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            
+            pageToken = data.get('nextPageToken')
+            if not pageToken:
+                break
+        return video_ids
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
-    print(f"Playlist ID: {playlist_id}")
-else:
-    print("get_playlist function not called")
+    video_ids = get_videos_ids(playlist_id)
